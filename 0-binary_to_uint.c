67278031bd8ea4f9b3670271969795a1c9ed4d5e
w-amon <xxx110680@gmail.com>
2023-09-29 08:41:59 +0300

    Add files

---
+++ 0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b) {
+    unsigned int result = 0;
+
+    if (b == NULL)
+        return 0;  
+
+    for (int i = 0; b[i] != '\0'; i++) {
+        if (b[i] != '0' && b[i] != '1')
+            return 0;  
+
+        result = (result << 1) | (b[i] - '0');
+    }
+
+    return result;
+}
+
+int main(void) {
+    unsigned int n;
+
+    n = binary_to_uint("1");
+    printf("%u\n", n);
+    n = binary_to_uint("101");
+    printf("%u\n", n);
+    n = binary_to_uint("1e01");
+    printf("%u\n", n);
+    n = binary_to_uint("1100010");
+    printf("%u\n", n);
+    n = binary_to_uint("0000000000000000000110010010");
+    printf("%u\n", n);
+
+    return 0;
+}
